ce6ba7dd4bed19085a139ecefa27d2ccf1854941
muhammedalfaris <muhammedalfariz8003@gmail.com>
2025-10-31 17:35:40 +0530

    port chnaged

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ app.use((err, req, res, next) => {
 
 // -------------------- Graceful shutdown --------------------
 // Why: Close DB connections on process termination to avoid open handles
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 const server = app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
